3e44d214126824a373ef362dc5ae39754dc584a6
Michael <morkrom@icloud.com>
2019-09-25 23:49:09 -0700

    make the call for photos for a given mars sol

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,8 @@ import Browser
 import Html exposing (Html, h1, h2, h3, h4, h5, img, br, div, text)
 import Html.Attributes exposing (src)
 import List exposing (..)
+import Dict exposing (Dict, diff)
+import String exposing (..) 
 
 -- network
 
@@ -17,15 +19,29 @@ import Json.Decode exposing (Decoder, field, map2, map3, string, list, dict, int
 
 import Url exposing (..)
 
+type alias Photo = {cameraKey: String, photoUrl: String}
 type alias Camera = {name: String, abbr: String}
 type alias Rover = {name: String, cameras: (List Camera), maxSol: Int}
-type alias DataModel = {rover: Rover, currentSol: Int}
+type alias DataModel = {rover: Rover, currentSol: Int, album: PhotoAlbum}
+
+type alias Sol = Int
+type alias CameraKey = String
+type alias PhotoRoll = Dict CameraKey (List Photo)
+type alias PhotoAlbum = Dict Sol PhotoRoll
+
+type alias FetchRoverResult = {rover: Rover, currentSol: Int}
+type alias FetchSolAlbumResult = List Photo
+--store: 
+-- << 'currentSol' did change
+-- sol : [camera_key : [CameraData]]
+-- > get sol: n, camera: camera_key  << "photos" : [{img_src: string}]
 
 type Model = Empty
            | Error String
            | Loaded DataModel
  
-type Msg = FetchRover (Result Http.Error DataModel)
+type Msg = FetchRover (Result Http.Error FetchRoverResult)
+         | FetchCameras (Result Http.Error FetchSolAlbumResult)
          | FetchError Http.Error
 
 subscriptions: Model -> Sub msg
@@ -34,54 +50,82 @@ subscriptions m = Sub.none
 bootUrl: String
 bootUrl = "https://mars-photos.herokuapp.com/api/v1/rovers/curiosity"
 
+-- By Mars Sol
+camerasUrl: Int -> String 
+camerasUrl sol = "https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?sol=" ++ (fromInt sol) 
+
 init: (Model, Cmd Msg)
 init = ( Empty 
        , fetchCuriosity )
 
 main = Browser.element{ init = \() -> init, view = view, update = update, subscriptions = subscriptions }
 
-fetchCuriosity : Cmd Msg 
+fetchCuriosity : Cmd Msg
 fetchCuriosity =
    Http.get { expect = Http.expectJson FetchRover roverEndpointDecoder, url = bootUrl }
 
--- > {} Loaded DataModel
--- > DataModel {rover: Rover, currentSol: Int, latestSol: Int}
+fetchCameras: Int -> Cmd Msg
+fetchCameras sol =
+  Http.get { expect = Http.expectJson FetchCameras camerasEndpointDecoder, url = camerasUrl sol}
 
-roverEndpointDecoder: Json.Decoder DataModel
+photoDecoder: Json.Decoder Photo 
+photoDecoder = 
+  map2 Photo (at ["camera", "name"] string) (field "img_src" string)
+
+camerasEndpointDecoder: Json.Decoder FetchSolAlbumResult
+camerasEndpointDecoder =
+  at ["photos"] (list photoDecoder)
+  --  
+
+roverEndpointDecoder: Json.Decoder FetchRoverResult
 roverEndpointDecoder = 
-  map2 DataModel (map3 Rover (at ["rover", "name"] string) (at ["rover", "cameras"] (list cameraDecoder)) (at ["rover", "max_sol"] int)) (at ["rover", "max_sol"] int)
-
--- "cameras"
+  map2 FetchRoverResult (map3 Rover (at ["rover", "name"] string) (at ["rover", "cameras"] (list cameraDecoder)) (at ["rover", "max_sol"] int)) (at ["rover", "max_sol"] int)
 
 cameraDecoder: Json.Decoder Camera
 cameraDecoder = 
   map2 Camera (field "full_name" string) (field "name" string) 
-  
--- "rover": object
--- max_sol: int
--- 
+
+camKeyMap: (List Photo) -> (List (String, Photo))
+camKeyMap list = 
+   List.map (\n -> (n.cameraKey, n)) list
+
+deTupleAndFlatten: String -> Photo -> Dict String (List Photo) -> Dict String (List Photo)
+deTupleAndFlatten key photo streamHead = 
+  Dict.update key (\_ -> Just ((Maybe.withDefault [] (Dict.get key streamHead)) ++ [photo])) streamHead
+
+photoRoll: (List Photo) -> PhotoRoll
+photoRoll list = 
+  Dict.foldl deTupleAndFlatten Dict.empty (Dict.fromList (camKeyMap list))
+
+photoAlbum: DataModel -> (List Photo) -> PhotoAlbum
+photoAlbum dataModel newPhotos =
+  Dict.update dataModel.currentSol (\_ -> Just (Dict.diff (Maybe.withDefault Dict.empty (Dict.get dataModel.currentSol dataModel.album)) (photoRoll newPhotos))) dataModel.album
 
 update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-      FetchRover result -> 
+      FetchRover result ->
+        case result of
+           Ok m ->
+             (Loaded {rover = m.rover, currentSol = m.currentSol, album = Dict.empty}, fetchCameras m.currentSol) 
+           Err _ ->
+             (Error "Big one", Cmd.none)   
+      FetchCameras result ->
         case result of
           Ok m ->
-            (Loaded m, Cmd.none)
-          
+            case model of 
+              Loaded current ->
+                (Loaded { current | album = (photoAlbum current m)}, Cmd.none)
+              _ ->
+                (Error "Expected model to be loaded by now, only trying to modify an existing model's photo album", Cmd.none)
           Err _ ->
-            (Error "Big one", Cmd.none)
-  
-      FetchError error ->
+            (Error "--", Cmd.none)
+      FetchError error -> 
         case error of
           Http.BadBody string ->
             (Error string, Cmd.none)
           _ -> 
             (Error "Unknown error", Cmd.none)
-
--- div for the camera image title, camera image subtitle, camera image
--- https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/latest_photos
--- image url 
 
 roverCameraImg: String -> Html msg
 roverCameraImg key = img [src "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00000/opgs/edr/fcam/FRA_397502305EDR_D0010000AUT_04096M_.JPG"] []
